b1faa2f47cebe7bbfdf92d2dc030ae15e11512f0
PauSubirada <psubirada23@gmail.com>
2025-02-07 06:05:42 +0100

    tenemos juego !!!!

--- Assets/Scripts/ImageTargetPosition.cs
+++ Assets/Scripts/ImageTargetPosition.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 public class ImageTargetPosition : MonoBehaviour
 {
     private float fixedY; // Almacena la posición inicial en Y
+    private float fixedX; // Almacena la posición inicial en X
 
     void Start()
     {
         fixedY = transform.position.y; // Guarda la posición inicial en Y
+        fixedX = transform.position.x; // Guarda la posición inicial en X
     }
 
     void Update()
     {
         // Mantiene la posición en Y fija mientras permite el movimiento en X y Z
-        transform.position = new Vector3(transform.position.x, fixedY, transform.position.z);
+        transform.position = new Vector3(fixedX, fixedY, transform.position.z);
     }
 }
